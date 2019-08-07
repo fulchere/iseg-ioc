d84282c01a52e6d1c3c34a7c40a5a810a81e1b92
Ethan Fulcher <fulchere@msu.edu>
2019-08-07 16:54:04 -0400

    missing isegHAL

--- iocBoot/ioc-iseg/st.cmd
+++ iocBoot/ioc-iseg/st.cmd
@@ -1,4 +1,4 @@
-#!../../bin/linux-x86_64/fths
+#!../../bin/linux-x86_64/iseg
 
 < envPaths
 
@@ -7,12 +7,14 @@ epicsEnvSet("STREAM_PROTOCOL_PATH", "$(TOP)")
 
 ## Register all support components
 dbLoadDatabase("${TOP}/dbd/iseg.dbd",0,0)
-fths_registerRecordDeviceDriver(pdbbase)
+iseg_registerRecordDeviceDriver(pdbbase)
+
+isegHalConnect("NAME","INTERFACE")
 
 drvAsynIPPortConfigure("dev1","192.168.1.101", 0, 0, 0)
 
 ## Load record instances
-dbLoadRecords("../../db/cryocon.db","P=LS1_CB04")
+dbLoadRecords("../../db/iseg.db")
 
 dbLoadRecords("$(EPICS_BASE)/db/asynRecord.db","P=,R=asyn,PORT=dev1,ADDR=0,OMAX=40,IMAX=40")
 
